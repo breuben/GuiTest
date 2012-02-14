69504cd480abb83ee415a99af72bae38c9e65e5e
breuben <breuben@gmail.com>
2012-02-13 17:43:38 -0800

    Moving right along.

--- GuiTest.cpp
+++ GuiTest.cpp
@@ -133,11 +133,13 @@ HWND CreateToolbarFromResource(HWND hWndParent)
 	InitCommonControlsEx(&icex);
 
 	// Declare and initialize local constants.
-	const int ImageListID    = 0;
-	const int numButtons     = 48;
-	const int bitmapSize     = 16;
+	int ImageListID;
+	int resourceId = IDB_FILE_VIEW_24;
+	int numButtons = 31;
+	const int bmHeight = 16;
+	const int bmWidth = 16;
 	
-	const DWORD buttonStyles = BTNS_AUTOSIZE;
+	const DWORD buttonStylesasdfasdf = BTNS_AUTOSIZE;
 
 	// Create the toolbar.
 	HWND hWndToolbar = CreateWindowEx(0, TOOLBARCLASSNAME, NULL, 
@@ -146,11 +148,19 @@ HWND CreateToolbarFromResource(HWND hWndParent)
 	if (hWndToolbar == NULL)
 		return NULL;
 
+
+	resourceId = IDB_TOOLBAR256;
+
+	HBITMAP hBitmap = LoadBitmap(hInst, MAKEINTRESOURCE(resourceId));
+
 	// Create the image list.
-	//hImageList = ImageList_Create(bitmapSize, bitmapSize, // Dimensions of individual bitmaps.
-									//ILC_COLOR16 | ILC_MASK, // Ensures transparent background.
-									//numButtons, 0);
-	hImageList = ImageList_LoadImage(hInst, MAKEINTRESOURCE(IDB_TOOLS16), bitmapSize, numButtons, CLR_DEFAULT, IMAGE_BITMAP, NULL);
+	int cx = bmWidth;
+	int cy = bmHeight;
+	UINT nFlags = ILC_COLOR24 | ILC_MASK;
+	int nInitial = 0;
+	int nGrow = 0;
+	hImageList = ImageList_Create(bmWidth, bmHeight, nFlags, 0, nGrow);
+	ImageListID = ImageList_AddMasked(hImageList, hBitmap, RGB(0, 255, 0));
 
 
 	// Set the image list.
@@ -172,19 +182,18 @@ HWND CreateToolbarFromResource(HWND hWndParent)
  
 	// Initialize button info.
 	// IDM_NEW, IDM_OPEN, and IDM_SAVE are application-defined command constants.
-	int buttonsToAdd = 9;
 	
 	::SendMessage(hWndToolbar, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
 	::SendMessage(hWndToolbar, TB_SETEXTENDEDSTYLE, 0, (LPARAM)TBSTYLE_EX_HIDECLIPPEDBUTTONS);
 
-	TBBUTTON *pButtons = new TBBUTTON[buttonsToAdd];
+	TBBUTTON *pButtons = new TBBUTTON[numButtons];
 
-	for (int i = 0 ; i < buttonsToAdd ; i++)
+	for (int i = 0 ; i < numButtons ; i++)
 	{
 		pButtons[i].iBitmap = i;
 		pButtons[i].idCommand = 20 + i;
-		pButtons[i].fsState = TBSTATE_ENABLED;
-		pButtons[i].fsStyle = TBSTYLE_FLAT;
+		pButtons[i].fsState = (BYTE)TBSTATE_ENABLED;
+		pButtons[i].fsStyle = (BYTE)TBSTYLE_FLAT;
 		pButtons[i].dwData = 0;
 		pButtons[i].iString = NULL;
 	}
@@ -203,12 +212,11 @@ HWND CreateToolbarFromResource(HWND hWndParent)
 	//    INT_PTR iString;
 	//} TBBUTTON, NEAR* PTBBUTTON, *LPTBBUTTON;
 
-	TBBUTTON b1;
 	BOOL b = IsAppThemed();
 
 	// Add buttons.
 	LRESULT a1 = SendMessage(hWndToolbar, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
-	LRESULT a2 = SendMessage(hWndToolbar, TB_ADDBUTTONS,       (WPARAM)buttonsToAdd,       (LPARAM)pButtons);
+	LRESULT a2 = SendMessage(hWndToolbar, TB_ADDBUTTONS,       (WPARAM)numButtons,       (LPARAM)pButtons);
 
 	// Resize the toolbar, and then show it.
 	LRESULT a3 = SendMessage(hWndToolbar, TB_AUTOSIZE, 0, 0); 
